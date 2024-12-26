eeba5ab3cf90f4149705ba8c8cca6408882fc19a
Mehzabin Haque <bsse1233@iit.du.ac.bd>
2024-12-26 23:14:46 +0600

    tsp updated

--- airbombing.cpp
+++ airbombing.cpp
@@ -1,7 +1,5 @@
 #include<iostream>
 #include<cstdio>
-#include<iostream>
-#include<cstdio>
 #include<vector>
 #include<queue>
 #include<queue>
--- tsp.cpp
+++ tsp.cpp
@@ -1,52 +1,55 @@
-#include<cstdio>
-#include<cmath>
-#include<vector>
-#include<iostream>
-#include<algorithm>
-#include<climits>
+#include <iostream>
+#include <vector>
+#include <climits>
 using namespace std;
 
-int solve(int n,vector<vector<int>> &matrix, int idx, int ini_mask, int total_mask){
-    if(ini_mask==total_mask){
-        return matrix[idx][0];
+const int INF = INT_MAX;
+
+int tsp(int pos, int mask, vector<vector<int>> &dist, vector<vector<int>> &dp, int N)
+{
+  if (mask == (1 << N) - 1)
+  {
+    return dist[pos][0];
+  }
+
+  if (dp[pos][mask] != -1)
+  {
+    return dp[pos][mask];
+  }
+
+  int ans = INF;
+
+  for (int next = 0; next < N; next++)
+  {
+    if ((mask & (1 << next)) == 0)
+    {
+      int newAns = dist[pos][next] + tsp(next, mask | (1 << next), dist, dp, N);
+      ans = min(ans, newAns);
     }
-    
-    int ans = INT_MAX;
-    
-    for(int i=0;i<n;i++){
-        if((ini_mask & (1<<i))==0){
-            int y = (ini_mask | (1<<i));
-            int temp = matrix[idx][i]+ solve(n,matrix,i,y,total_mask);
-            ans = min(ans,temp);
-        }
-    }
-    
-    return ans;
+  }
+
+  return dp[pos][mask] = ans;
 }
 
-int main(){
-    
-    int t;
-    cin>>t;
-    
-   
-    
-    for(int i=0;i<t;i++){
-        int n;
-        cin>>n;
-        vector<vector<int>> matrix(n,vector<int>(n));
-        
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                cin>>matrix[i][j];
-            }
-        }
-        
-        int ini_mask=0;
-        int total_mask=(1<<n)-1;
-        cout<<solve(n,matrix,0,ini_mask,total_mask)<<endl;
+int main()
+{
+  int T;
+  cin >> T;
+  while (T--)
+  {
+    int N;
+    cin >> N;
+    vector<vector<int>> dist(N, vector<int>(N));
+
+    for (int i = 0; i < N; i++)
+    {
+      for (int j = 0; j < N; j++)
+      {
+        cin >> dist[i][j];
+      }
     }
-    
-   
-    return 0;
+
+    vector<vector<int>> dp(N, vector<int>(1 << N, -1));
+    cout << tsp(0, 1, dist, dp, N) << endl;
+  }
 }
