3a1bf2605fb82b9d489683be05a29def82c7af0d
Mehzabin Haque <74411032+mehzabin-haque@users.noreply.github.com>
2025-01-06 15:10:27 +0600

    Update flip.cpp

--- flip.cpp
+++ flip.cpp
@@ -1,53 +1,45 @@
-#include <iostream>
-#include <vector>
-#include <unordered_map>
-#include <string>
-#include <algorithm>
+#include<bits/stdc++.h>
 using namespace std;
 
-int main() {
-    int n, m, k;
+int main()
+{
+    int n,m,k, ans=INT_MIN;
 
-    // Input variables
-    cin >> n >> m >> k;
-    vector<vector<int>> mat(n, vector<int>(m));
+    cin>>n>>m>>k;
 
-    // Inputting the matrix
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < m; j++) {
-            cin >> mat[i][j];
+    unordered_map<string,int>freq;
+
+    for(int i=0; i<n;i++)
+    {
+        int zeros = 0;
+
+        string temp = "";
+        for (int j=0;j<m;j++)
+        {
+            int x;
+            cin>>x;
+            if(x==0)
+            {
+                zeros++;
+            }
+            temp += '0'+x;
+        }
+
+        if(zeros<=k && (k-zeros)%2==0)
+        {
+            freq[temp]++;
         }
     }
 
-    unordered_map<string, int> frequencyMap;
-
-    // Storing frequency of each row configuration in the map
-    for (int i = 0; i < n; i++) {
-        string rowStr = "";
-        for (int j = 0; j < m; j++) {
-            rowStr += to_string(mat[i][j]);
-        }
-        frequencyMap[rowStr]++;
-    }
-
-    int maxRows = 0;
-
-    // Iterate through the map
-    for (const auto &entry : frequencyMap) {
-        string rowStr = entry.first;
-        int freq = entry.second;
-
-        // Count number of zeros in the row string
-        int num_zeros = count(rowStr.begin(), rowStr.end(), '0');
-
-        // Check conditions: num_zeros <= k and (k - num_zeros) % 2 == 0
-        if (num_zeros <= k && (k - num_zeros) % 2 == 0) {
-            maxRows = max(maxRows, freq);
+    for(auto it: freq)
+    {
+        if(it.second>ans)
+        {
+            ans=it.second;
         }
     }
 
-    // Output the result
-    cout << maxRows << endl;
+    cout<<ans<<endl;
 
     return 0;
 }
